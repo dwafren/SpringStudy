1b0eeb2ab7681551ad8d23e5bff8d0677c82c2a0
Hao Ren <dwarfrenster@gmail.com>
2019-06-18 22:15:44 -0700

    add demo spring

---
+++ demo/src/test/java/haoren/springbase/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package haoren.springbase.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
